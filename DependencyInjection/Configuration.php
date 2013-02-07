af2feb0994f2850d9ddf6d2597be3c5b082ef5ef
Sebastian Grodzicki <sebastian@grodzicki.pl>
2013-02-07 16:18:03 +0100

    Update DependencyInjection/Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,6 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
+        $treeBuilder->root('gilles_hack_session');
 
         return $treeBuilder;
     }
